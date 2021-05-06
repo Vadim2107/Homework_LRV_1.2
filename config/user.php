ccdac9a0ca715d367087ae348a32f212ffc342bf
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2021-05-06 19:19:19 +0300

    first commit

---
+++ config/user.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'env' => env('APP_ENV', 'dev'),
+    'name' => env('NAME', 'Vadim'),
+    'timezone' => 'Europe/Moscow',
+    'locale' => 'ru'
+];
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\User;
+use Illuminate\Support\Facades\View;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/', function () {
+////    return view('user', ['name' => 'Vadik']);
+//    return view('user', ['name' => env('NAME', 'Vadim')]);
+//});
